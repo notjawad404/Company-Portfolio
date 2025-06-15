e4fe721b77a2302dc1feab41b47a37850cee535e
Jawad Ali <jawadali000123@gmail.com>
2025-06-15 23:22:33 +0500

    Initial Setup

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Header from "./components/Header";
+import Navbar from "./components/navbar";
+
+
+export default function App() {
+  return (
+    <div>
+      <Navbar/>
+      <Header/>
+    </div>
+  )
+}
